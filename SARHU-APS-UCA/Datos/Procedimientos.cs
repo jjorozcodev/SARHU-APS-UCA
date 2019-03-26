e44e30a4521c4c576c691944246bb3560f3c7eb4
JJ <jj.orozcolopez.13@gmail.com>
2019-03-26 09:12:31 -0600

    Iter#2: Mejoras a Updatepanel y Script SQL
    Actualización de tablas y procedimientos almacenados del script de BD.
    Mejoras al UpdatePanel para Departamentos y Municipios (Script Ajax Microsoft).
    Actualización y corrección de nombres de procedimientos almacenados.

--- SARHU-APS-UCA/Datos/Procedimientos.cs
+++ SARHU-APS-UCA/Datos/Procedimientos.cs
@@ -3,84 +3,127 @@
     public class Procedimientos
     {
         // ORGANIZACIÓN
-        public static string OrganizacionObtener = "[dbo].[sp_organizacion_consult]";
-        public static string OrganizacionEditar = "[dbo].[sp_organizacion_update]";
+        public static string OrganizacionObtener = "[sp_organizacion_consult]";
+        public static string OrganizacionEditar = "[sp_organizacion_update]";
 
         // BITÁCORA
-        public static string BitacoraListar = "[dbo].[sp_bitacora_list]";
-        public static string BitacoraInsertar = "[dbo].[sp_bitacora_insert]";
+        public static string BitacoraListar = "[sp_bitacora_list]";
+        public static string BitacoraInsertar = "[sp_bitacora_insert]";
 
         // DEPARTAMENTOS
-        public static string DepartamentosListar = "[dbo].[sp_departamentos_list]";
+        public static string DepartamentosListar = "[sp_departamentos_list]";
 
         // MUNICIPIOS
-        public static string MunicipiosListar = "[dbo].[sp_municipios_list]";
+        public static string MunicipiosListar = "[sp_municipios_list]";
 
         // PROGRAMAS
-        public static string ProgramasListar = "[dbo].[sp_programas_list]";
-        public static string ProgramasConsultar = "[dbo].[sp_programas_consult]";
-        public static string ProgramasAgregar = "[dbo].[sp_programas_insert]";
-        public static string ProgramasEditar = "[dbo].[sp_programas_update]";
-        public static string ProgramasBorrar = "[dbo].[sp_programas_delete]";
+        public static string ProgramasListar = "[sp_programas_list]";
+        public static string ProgramasConsultar = "[sp_programas_consult]";
+        public static string ProgramasAgregar = "[sp_programas_insert]";
+        public static string ProgramasEditar = "[sp_programas_update]";
+        public static string ProgramasBorrar = "[sp_programas_delete]";
         
         // LOCALIDADES
-        public static string LocalidadesListar = "[dbo].[sp_localidades_list]";
-        public static string LocalidadesConsultar = "[dbo].[sp_localidades_consult]";
-        public static string LocalidadesAgregar = "[dbo].[sp_localidades_insert]";
-        public static string LocalidadesEditar = "[dbo].[sp_localidades_update]";
-        public static string LocalidadesBorrar = "[dbo].[sp_localidades_delete]";
+        public static string LocalidadesListar = "[sp_localidades_list]";
+        public static string LocalidadesConsultar = "[sp_localidades_consult]";
+        public static string LocalidadesAgregar = "[sp_localidades_insert]";
+        public static string LocalidadesEditar = "[sp_localidades_update]";
+        public static string LocalidadesBorrar = "[sp_localidades_delete]";
 
         // ROLES
-        public static string RolesListar = "[dbo].[sp_roles_list]";
-        public static string RolesConsultar = "[dbo].[sp_roles_consult]";
-        public static string RolesAgregar = "[dbo].[sp_roles_insert]";
-        public static string RolesEditar = "[dbo].[sp_roles_update]";
-        public static string RolesBorrar = "[dbo].[sp_roles_delete]";
+        public static string RolesListar = "[sp_roles_list]";
+        public static string RolesConsultar = "[sp_roles_consult]";
+        public static string RolesAgregar = "[sp_roles_insert]";
+        public static string RolesEditar = "[sp_roles_update]";
+        public static string RolesBorrar = "[sp_roles_delete]";
+
+        // USUARIOS
+        public static string UsuariosListar = "[sp_usuarios_list]";
+        public static string UsuariosConsultar = "[sp_usuarios_consult]";
+        public static string UsuariosAgregar = "[sp_usuarios_insert]";
+        public static string UsuariosEditar = "[sp_usuarios_update]";
+        public static string UsuariosBorrar = "[sp_usuarios_delete]";
+
+        // PERMISOS
+        public static string PermisosListar = "[sp_permisos_list]";
+        public static string PermisosAgregar = "[sp_permisos_insert]";
+        public static string PermisosBorrar = "[sp_permisos_delete]";
 
         // ÁREAS
-        public static string AreasListar = "[dbo].[sp_areas_list]";
-        public static string AreasConsultar = "[dbo].[sp_areas_consult]";
-        public static string AreasAgregar = "[dbo].[sp_areas_insert]";
-        public static string AreasEditar = "[dbo].[sp_areas_update]";
-        public static string AreasBorrar = "[dbo].[sp_areas_delete]";
+        public static string AreasListar = "[sp_areas_list]";
+        public static string AreasConsultar = "[sp_areas_consult]";
+        public static string AreasAgregar = "[sp_areas_insert]";
+        public static string AreasEditar = "[sp_areas_update]";
+        public static string AreasBorrar = "[sp_areas_delete]";
+
+        // PUESTOS
+        public static string PuestosListar = "[sp_puestos_list]";
+        public static string PuestosConsultar = "[sp_puestos_consult]";
+        public static string PuestosAgregar = "[sp_puestos_insert]";
+        public static string PuestosEditar = "[sp_puestos_update]";
+        public static string PuestosBorrar = "[sp_puestos_delete]";
+
+        // ADENDUMS
+        public static string AdendumsListar = "[sp_adendums_list]";
+        public static string AdendumsConsultar = "[sp_adendums_consult]";
+        public static string AdendumsAgregar = "[sp_adendums_insert]";
+        public static string AdendumsEditar = "[sp_adendums_update]";
+        public static string AdendumsBorrar = "[sp_adendums_delete]";
 
         // FUNCIONES
-        public static string FuncionesListar = "[dbo].[sp_funciones_list]";
-        public static string FuncionesConsultar = "[dbo].[sp_funciones_consult]";
-        public static string FuncionesAgregar = "[dbo].[sp_funciones_insert]";
-        public static string FuncionesEditar = "[dbo].[sp_funciones_update]";
-        public static string FuncionesBorrar = "[dbo].[sp_funciones_delete]";
+        public static string FuncionesListar = "[sp_funciones_list]";
+        public static string FuncionesConsultar = "[sp_funciones_consult]";
+        public static string FuncionesAgregar = "[sp_funciones_insert]";
+        public static string FuncionesEditar = "[sp_funciones_update]";
+        public static string FuncionesBorrar = "[sp_funciones_delete]";
+
+        // PUESTO-FUNCIONES
+        public static string PuestoFuncionesListar = "[sp_puesto_funcion_list]";
+        public static string PuestoFuncionesAgregar = "[sp_puesto_funcion_insert]";
+        public static string PuestoFuncionesBorrar = "[sp_puesto_funcion_delete]";
+
+        // ADENDUM-FUNCIONES
+        public static string AdendumFuncionesListar = "[sp_adendum_funcion_list]";
+        public static string AdendumFuncionesAgregar = "[sp_adendum_funcion_insert]";
+        public static string AdendumFuncionesBorrar = "[sp_adendum_funcion_delete]";
 
         // BONOS
-        public static string BonosListar = "[dbo].[sp_bonos_list]";
-        public static string BonosConsultar = "[dbo].[sp_bonos_consult]";
-        public static string BonosAgregar = "[dbo].[sp_bonos_insert]";
-        public static string BonosEditar = "[dbo].[sp_bonos_update]";
-        public static string BonosBorrar = "[dbo].[sp_bonos_delete]";
+        public static string BonosListar = "[sp_bonos_list]";
+        public static string BonosConsultar = "[sp_bonos_consult]";
+        public static string BonosAgregar = "[sp_bonos_insert]";
+        public static string BonosEditar = "[sp_bonos_update]";
+        public static string BonosBorrar = "[sp_bonos_delete]";
+
+        // ADELANTOS
+        public static string AdelantosListar = "[sp_adelantos_list]";
+        public static string AdelantosConsultar = "[sp_adelantos_consult]";
+        public static string AdelantosAgregar = "[sp_adelantos_insert]";
+        public static string AdelantosEditar = "[sp_adelantos_update]";
+        public static string AdelantosBorrar = "[sp_adelantos_delete]";
 
         // IR
-        public static string IRListar = "[dbo].[sp_ir_list]";
-        public static string IRConsultar = "[dbo].[sp_ir_consult]";
-        public static string IREditar = "[dbo].[sp_ir_update]";
+        public static string IRListar = "[sp_ir_list]";
+        public static string IRConsultar = "[sp_ir_consult]";
+        public static string IREditar = "[sp_ir_update]";
 
         // INSS
-        public static string INSSListar = "[dbo].[sp_inss_list]";
-        public static string INSSConsultar = "[dbo].[sp_inss_consult]";
-        public static string INSSEditar = "[dbo].[sp_inss_update]";
+        public static string INSSListar = "[sp_inss_list]";
+        public static string INSSConsultar = "[sp_inss_consult]";
+        public static string INSSEditar = "[sp_inss_update]";
 
         // VARIABLES
-        public static string VariablesListar = "[dbo].[sp_variables_list]";
-        public static string VariablesConsultar = "[dbo].[sp_variables_consult]";
-        public static string VariablesEditar = "[dbo].[sp_variables_update]";
+        public static string VariablesListar = "[sp_variables_list]";
+        public static string VariablesConsultar = "[sp_variables_consult]";
+        public static string VariablesEditar = "[sp_variables_update]";
 
         // MODULOS
-        public static string ModulosListar = "[dbo].[sp_modulos_list]";
+        public static string ModulosListar = "[sp_modulos_list]";
 
         // FUNCIONALIDADES
-        public static string FuncionalidadesListar = "[dbo].[sp_funcionalidades_list]";
+        public static string FuncionalidadesListar = "[sp_funcionalidades_list]";
 
         // OPERACIONES
-        public static string OperacionesListar = "[dbo].[sp_operaciones_list]";
+        public static string OperacionesListar = "[sp_operaciones_list]";
 
     }
 }
