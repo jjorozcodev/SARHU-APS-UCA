2e5872e16ee249440030d1be018fb09dd012488b
skadied <eddymoreno10@gmail.com>
2019-04-17 10:48:36 -0600

    added validations to model

--- SARHU-APS-UCA/Entidades/Puestos.cs
+++ SARHU-APS-UCA/Entidades/Puestos.cs
@@ -9,10 +9,22 @@ namespace Entidades
     public class Puestos
     {
         public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 5)]
+        [Required]
         public string Nombre { get; set; }
+        
+        [StringLength(150, MinimumLength = 5)]
         public string Descripcion { get; set; }
+
+        [Required]
         public int CuentaId { get; set; }
+        
+        [Required]
         public int AreaId { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(9, 2)")]
         public decimal SalarioBase { get; set; }
 
     }
