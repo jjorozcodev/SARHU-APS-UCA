fe2eaa0980a60d49a781f4a7bff1d388e0db1551
JJ <jj.orozcolopez.13@gmail.com>
2019-04-20 12:42:27 -0600

    Intentando validar desde el modelo
    Validaciona modelo PUESTOS en prueba.

--- SARHU-APS-UCA/Entidades/Puesto.cs
+++ SARHU-APS-UCA/Entidades/Puesto.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Entidades
 {
     public class Puesto
     {
         public int Id { get; set; }
+
+        [StringLength(50, MinimumLength = 5)]
+        [Required]
         public string Nombre { get; set; }
+
+        [StringLength(150, MinimumLength = 5)]
         public string Descripcion { get; set; }
+
+        [Required]
         public int CuentaId { get; set; }
+
+        [Required]
         public int AreaId { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(9, 2)")]
         public decimal SalarioBase { get; set; }
         public bool Estado { get; set; }
 
